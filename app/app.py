d3a9f27bcbdfd7cee70a17b1e5dfcf36501c7fc5
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-11-28 16:19:12 -0300

    Add classes and members page

--- app/app.py
+++ app/app.py
@@ -3,10 +3,35 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def landing_page(name=None):
-    return render_template('index.html', name=name)
+def landing_page():
+    return render_template('index.html')
 
-# @app.route('/hello/')
-# @app.route('/hello/<name>')
-# def hello(name=None):
-#     return render_template('index.html', name=name)
+@app.route('/members')
+def members():
+    members_data = [
+        ({
+            "name": "Arthur",
+            "id": "12352352",
+            "picture": "https://randomuser.me/api/portraits/men/33.jpg",
+            "is_premium": False
+        },
+        {
+            "name": "Alves",
+            "id": "63546352",
+            "picture": "https://randomuser.me/api/portraits/men/75.jpg",
+            "is_premium": True
+        })
+    ]
+
+    return render_template('members.html', members=members_data)
+
+@app.route('/members/<id>')
+def member_workout(id=None):
+    if id == None:
+        return members()
+
+    return render_template('workout.html', id=id)
+
+@app.route('/classes')
+def classes():
+    return render_template('classes.html')
