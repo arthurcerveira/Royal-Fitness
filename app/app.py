5950829ecc3108467c0330ca36a2ff3caafe4bbb
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-11-29 15:16:15 -0300

    Add routine and classes page

--- app/app.py
+++ app/app.py
@@ -2,36 +2,103 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+members_data = {
+    "12352352": {
+        "name": "Brad Gibson",
+        "id": "12352352",
+        "picture": "https://randomuser.me/api/portraits/men/33.jpg",
+        "is_premium": False
+    },
+    "63546352": {
+        "name": "Vincent Fletcher",
+        "id": "63546352",
+        "picture": "https://randomuser.me/api/portraits/men/75.jpg",
+        "is_premium": True
+    }
+}
+
+routine_data = {
+    "12352352": {
+        "monday": [{
+            "name": "Bench Press",
+            "repetitions": 5,
+            "sets": 5,
+            "weight": 70,
+            "type": 'strenght'
+        }],
+        "tuesday": None,
+        "wednesday": [{
+            "name": "Bench Press",
+            "repetitions": 5,
+            "sets": 5,
+            "weight": 70,
+            "type": 'strenght'
+        },{
+            "name": "Treadmill",
+            "duration": 30,
+            "intensity": 8,
+            "type": 'cardio'
+        }],
+        "thursday": None,
+        "friday": None,
+        "saturday": None
+    },
+    "63546352": {
+        "name": "Vincent Fletcher",
+        "id": "63546352",
+        "picture": "https://randomuser.me/api/portraits/men/75.jpg",
+        "is_premium": True
+    }
+}
+
+classes_data = {
+    "1234": {
+        "name": "yoga",
+        "description": "Yoga class for people interested in yoga.",
+        "image": "yoga-class.jpeg",
+        "day": "monday",
+        "time": "8:30"
+    },
+    "4567": {
+        "name": "boxing",
+        "description": "Boxing class for people interested in boxing.",
+        "image": "boxing-class.jpeg",
+        "day": "thurday",
+        "time": "15:30"
+    }
+}
+
 @app.route('/')
 def landing_page():
     return render_template('index.html')
 
 @app.route('/members')
 def members():
-    members_data = [
-        ({
-            "name": "Arthur",
-            "id": "12352352",
-            "picture": "https://randomuser.me/api/portraits/men/33.jpg",
-            "is_premium": False
-        },
-        {
-            "name": "Alves",
-            "id": "63546352",
-            "picture": "https://randomuser.me/api/portraits/men/75.jpg",
-            "is_premium": True
-        })
-    ]
+    members_array = list()
+    member_tuple = list()
 
-    return render_template('members.html', members=members_data)
+    for member in members_data.values():
+        member_tuple.append(member)
 
-@app.route('/members/<id>')
-def member_workout(id=None):
-    if id == None:
+        if len(member_tuple) == 2:
+            members_array.append(member_tuple.copy())
+            member_tuple.clear()
+
+    return render_template('members.html', members=members_array)
+
+@app.route('/routine/<member_id>')
+def member_workout(member_id=None):
+    if member_id == None:
         return members()
+    
+    member = members_data[member_id]
+    routine = routine_data[member_id]
 
-    return render_template('workout.html', id=id)
+    return render_template('routine.html', member=member, routine=routine)
 
 @app.route('/classes')
 def classes():
-    return render_template('classes.html')
+    return render_template('classes.html', classes=classes_data.values())
+
+if __name__ == '__main__':
+    app.run(debug=True)
